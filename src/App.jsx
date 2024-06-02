972794929c0b820cb0098d359eabc2b813721b70
Abhinav <abhinavajay20@gmail.com>
2024-06-02 20:19:33 +0530

    Creating header/Navbar Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from './Pages/Home'
 import Profile from './Pages/Profile'
 import About from './Pages/About'
 import SignUp from './Pages/SignUp'
+import Header from './Components/Header'
 import Signin from './Pages/Signin'
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element = {<Home/>}></Route>
         <Route path='/profile' element = {<Profile/>}></Route>
