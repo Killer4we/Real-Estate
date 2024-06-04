ec642d970c3d9bbd5fd94ddf6daf9f6a9d93b699
Abhinav <abhinavajay20@gmail.com>
2024-06-04 22:58:21 +0530

    connecting to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
-
+mongoose.connect("mongodb+srv://abhinavajay20:YTlDkRoyR3y23uu4@cluster0.xowutff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+    console.log("Connected to Databse");
+}).catch((err)=>{
+    console.log("Error connecting to the database"); 
+});
 const portNo = 3000;
 app.listen(portNo,()=>{
     console.log("Server is running on port",portNo);
