deaf82e86233321153bc617f3b48d5bc89ca7536
Abhinav <abhinavajay20@gmail.com>
2024-06-06 12:45:16 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import authRouter from "./routes/auth.route.js";
+
 const app = express();
 mongoose.connect("mongodb+srv://abhinavajay20:YTlDkRoyR3y23uu4@cluster0.xowutff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
-    console.log("Connected to Databse");
+    console.log("Connected to Database");
 }).catch((err)=>{
     console.log("Error connecting to the database"); 
 });
@@ -10,3 +12,5 @@ const portNo = 3000;
 app.listen(portNo,()=>{
     console.log("Server is running on port",portNo);
 });
+
+app.use('/api/auth',authRouter);
