47bd2584e411b285d7add35a02c5f50a178c119e
Abhinav <abhinavajay20@gmail.com>
2024-06-10 12:35:09 +0530

    updating the navbar and profile section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,16 @@ import About from './Pages/About'
 import SignUp from './Pages/SignUp'
 import Header from './Components/Header'
 import Signin from './Pages/Signin'
+import PrivateRoute from './Components/PrivateRoute'
 export default function App() {
   return (
     <BrowserRouter>
       <Header/>
       <Routes>
         <Route path='/' element = {<Home/>}></Route>
-        <Route path='/profile' element = {<Profile/>}></Route>
+        <Route element = {<PrivateRoute/>}>
+          <Route path='/profile' element = {<Profile/>}></Route>
+        </Route>
         <Route path='/about' element = {<About/>}></Route>
         <Route path='/sign-up' element = {<SignUp/>}></Route>
         <Route path='/sign-in' element = {<Signin/>}></Route>
