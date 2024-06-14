f935eff53ec55320b67f594555c719cade1b850b
Abhinav <abhinavajay20@gmail.com>
2024-06-14 12:05:19 +0530

    created a listing model and also a createListing api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -19,6 +20,7 @@ app.listen(portNo,()=>{
 });
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
+app.use('/api/listing',listingRouter);
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode||500;
