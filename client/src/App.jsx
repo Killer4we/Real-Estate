e89c54eb8c76d93424f6f8bfe862d06247d915b1
Abhinav <abhinavajay20@gmail.com>
2024-06-18 14:49:31 +0530

    created an update user listing route and completed its functionality with frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import Profile from './Pages/Profile'
 import About from './Pages/About'
 import SignUp from './Pages/SignUp'
+import UpdateListing from './Pages/UpdateListing'
 import Header from './Components/Header'
 import Signin from './Pages/Signin'
 import PrivateRoute from './Components/PrivateRoute'
@@ -17,6 +18,7 @@ export default function App() {
         <Route element = {<PrivateRoute/>}>
           <Route path='/profile' element = {<Profile/>}/>
           <Route path='/create-listing' element = {<CreateListing/>}/>
+          <Route path='/update-listing/:listingId' element = {<UpdateListing/>}/>
         </Route>
         <Route path='/about' element = {<About/>}></Route>
         <Route path='/sign-up' element = {<SignUp/>}></Route>
