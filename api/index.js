d5b3e2f767a69d6661cd2654e3adae851053d44e
Abhinav <abhinavajay20@gmail.com>
2024-06-21 16:50:12 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -4,13 +4,17 @@ import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-
+import path from 'path';
 const app = express();
 mongoose.connect("mongodb+srv://abhinavajay20:YTlDkRoyR3y23uu4@cluster0.xowutff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log("Connected to Database");
 }).catch((err)=>{
     console.log("Error connecting to the database"); 
 });
+
+const __dirname = path.resolve();
+
+
 const portNo = 3000;
 app.use(express.json());
 app.use(cookieParser());
@@ -21,6 +25,14 @@ app.listen(portNo,()=>{
 app.use('/api/auth',authRouter);
 app.use('/api/user',userRouter);
 app.use('/api/listing',listingRouter);
+
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
+
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode||500;
